ba09294ec10b172897a5dcc7c46a1091ee256d8b
Fredrik Tornell <fredrik.tornell@gmail.com>
2018-04-09 15:42:40 +0200

    Update

--- app/functions.php
+++ app/functions.php
@@ -2,4 +2,6 @@
 
 // General functions
 $siteName = $db->query("SELECT sitename FROM arc_settings")->fetchColumn();
+
+// Alert handler
 $alerts = $db->query("SELECT alerts FROM arc_settings")->fetchColumn();
